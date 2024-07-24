4102853b93aef0595a27d9b3faf8b6e3df580ff0
Mrsmartchoice <133871052+mrsmartchoice@users.noreply.github.com>
2024-07-24 03:09:27 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/* if you find this source code helpul,Feel free to support our channel 
+ on Buymecoffee page and get access to more login forms 
+ and web projects. link below! 
+ https://buymeacoffee.com/mrsmartchoice  */
+
